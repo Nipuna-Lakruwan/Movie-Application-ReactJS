25dcde25d9835ef05cbe612a55eaf1a2188de1af
Nipuna Lakruwan <laknipsa@gmail.com>
2025-02-01 20:46:19 +0530

    refactor App component to include Search functionality and update layout structure

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -1,29 +1,26 @@
-import { useState } from 'react'
-
-const Card = ({ title }) => {
-  const [hasLiked, setHasLiked] = useState(false);
-  return (
-    <div className="card" >
-      <h2>{title}</h2>
-
-      <button onClick={() => setHasLiked(!hasLiked)}>
-        {hasLiked ? 'Liked' : 'Like'}
-      </button>
-    </div>
-  )
-}
+import React from 'react'
+import Search from './components/Search'
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = React.useState(''); // Create a state variable to store the search term entered by the user in the search input field and a function to update the state variable.
 
 
   return (
-    <div>
-    <h2>Functional Arrow Component</h2>
+    <main>
 
-    <Card title="Stars Wars" rating={5} isCool={true} actors={[{ name: 'Actors'}]} />
-    <Card title="Avatar" />
-    <Card title="The Lion King" />
-    </div>
+      <div className='pattern' >
+
+        <div className='wrapper'>
+            <header>
+              <img src="./hero.png" alt='Hero Banner' />
+              <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle</h1>
+            </header>
+
+            <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+            <h1 className='text-white'>{searchTerm}</h1>
+        </div>
+      </div>
+    </main>
   )
 }
 
